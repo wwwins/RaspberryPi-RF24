ea58e49ef781d59f429bca5d003797ca04eb5e60
wwwins <jacky.huang@isobar.com>
2016-01-19 13:02:58 +0800

    Added count number.

--- receiver.py
+++ receiver.py
@@ -7,6 +7,9 @@ from RF24 import *
 
 radio = RF24(RPI_BPLUS_GPIO_J8_15, RPI_BPLUS_GPIO_J8_24, BCM2835_SPI_SPEED_8MHZ)
 
+count_of_write = 0
+count_of_read = 0
+
 def setup():
   print('<<receiver>>')
   pipes = [0xABCDABCD71, 0x544d52687C]
@@ -24,18 +27,30 @@ def setup():
   radio.startListening()
 
 def sendCallback(msg):
+  global count_of_write
   radio.stopListening()
+  count_of_write = count_of_write+1
   radio.write(msg)
   print('Send Response:',msg)
   radio.startListening()
 
-
+def main():
+  global count_of_read
+  while 1:
+    while radio.available():
+      count_of_read = count_of_read+1
+      receive_payload = radio.read(32)
+      print('Received:',receive_payload.decode('utf-8'))
+      sendCallback(receive_payload)
+      time.sleep(0.1)
 
 setup()
 
-while 1:
-  while radio.available():
-    receive_payload = radio.read(32)
-    print('Received:',receive_payload.decode('utf-8'))
-    sendCallback(receive_payload)
-    time.sleep(0.1)
+try:
+  if __name__ == "__main__":
+    main()
+
+finally:
+  print("\n=================================================")
+  print("Receive:",count_of_read,"Response:",count_of_write)
+  print("=================================================")
